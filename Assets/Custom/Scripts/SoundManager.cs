83824f948709972d2e070da4f2ba4a30ffc12215
Toby Morgan <tmo89@uclive.ac.nz>
2023-05-22 14:55:37 +1200

    Nearly finished Lab 9

--- Assets/Custom/Scripts/SoundManager.cs
+++ Assets/Custom/Scripts/SoundManager.cs
@@ -9,14 +9,35 @@ public class SoundManager : MonoBehaviour
     [SerializeField]
     private AudioClip music;
 
+    [SerializeField]
+    private float firstBeatOffset;
+
+    public float BPM;
+
+    public float positionInBeats;
+
+    private float secsPerBeat;
+
+    private float positionInSecs;
+
+    //Time when music starts playing
+    private float dspSongTime;
+
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        secsPerBeat = 60f / BPM;
+        //Record time when the music starts
+        dspSongTime = (float)AudioSettings.dspTime;
         PlayMusic();
     }
 
-    //TODO Currently plays music on Startup
+    void Update() {
+        positionInSecs = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
+        positionInBeats = songPosition / secsPerBeat;
+    }
+
     //Plays the music for the game
     public void PlayMusic()
     {
